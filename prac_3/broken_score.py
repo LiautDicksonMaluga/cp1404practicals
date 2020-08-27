40081cd8c8b0be3a0b758ffa592f401b85365254
Maluga-coding <liaut.dicksonmaluga@my.jcu.edu.au>
2020-08-27 13:54:54 +1000

    Refactor broken score program to use functions

--- prac_3/broken_score.py
+++ prac_3/broken_score.py
@@ -4,15 +4,29 @@ Broken program to determine score status
 """
 
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
-        print("Invalid score")
-    if score > 90:
-        print("Excellent")
-    if score > 50:
-        print("Passable")
-    if score < 50:
-        print("Bad")
+def main():
+    score = get_user_score()
+    result = return_score_result(score)
+    print(result)
+
+
+def return_score_result(score):
+    if score < 0:
+        return "Invalid score"
+    else:
+        if score > 100:
+            return "Invalid score"
+        if score > 90:
+            return "Excellent"
+        if score > 50:
+            return "Passable"
+        if score < 50:
+            return "Bad"
+
+
+def get_user_score():
+    score = float(input("Enter score: "))
+    return score
+
+
+main()
