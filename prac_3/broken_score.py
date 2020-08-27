5ca6e524e2cd3126345c4ee13f0689698eadf737
Maluga-coding <liaut.dicksonmaluga@my.jcu.edu.au>
2020-08-27 14:15:11 +1000

    Added random score to broken score

--- prac_3/broken_score.py
+++ prac_3/broken_score.py
@@ -2,12 +2,16 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
 
 def main():
     score = get_user_score()
     result = return_score_result(score)
     print(result)
+    random_score = random.randint(0, 100)
+    random_result = return_score_result(random_score)
+    print("Random score is {} which is {}".format(random_score, random_result))
 
 
 def return_score_result(score):
@@ -27,6 +31,7 @@ def return_score_result(score):
 def get_user_score():
     score = float(input("Enter score: "))
     return score
+
 
 
 main()
