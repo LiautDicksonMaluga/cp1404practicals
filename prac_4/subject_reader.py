d21f2ae0a8095cd6630cc7ec9275a06410819d39
Maluga-coding <liaut.dicksonmaluga@my.jcu.edu.au>
2020-09-01 15:37:29 +1000

    Reformated subject_reader.py to include functions

--- prac_4/subject_reader.py
+++ prac_4/subject_reader.py
@@ -8,22 +8,26 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    display_data(data)
+
+
+def display_data(data):
+    for i in data:
+        print("{} is taught by {:<12} and has {:>3} students".format(i[0], i[1], i[2]))
+    return
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    lists = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        lists.append(parts)
     input_file.close()
+    return lists
 
 
 main()
