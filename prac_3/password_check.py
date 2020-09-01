5923ad57377d043054984ef1e064d179146d50cb
Maluga-coding <liaut.dicksonmaluga@my.jcu.edu.au>
2020-09-01 14:26:43 +1000

    Added walkthrough example and warmup

--- prac_3/password_check.py
+++ prac_3/password_check.py
@@ -1,19 +1,22 @@
-def main():
-    password = get_password()
-    while len(password) < 4:
-        print("Password does not meet the minimum requirements")
-        password = input("What is your password: ")
-    print_asteriks(password)
+# def main():
+#     password = get_password()
+#     while len(password) < 4:
+#         print("Password does not meet the minimum requirements")
+#         password = input("What is your password: ")
+#     print_asteriks(password)
+#
+#
+# def print_asteriks(password):
+#     for i in range(0, len(password)):
+#         print("*", end="")
+#
+#
+# def get_password():
+#     password = str(input("What is your password: "))
+#     return password
+#
+#
+# main()
 
-
-def print_asteriks(password):
-    for i in range(0, len(password)):
-        print("*", end="")
-
-
-def get_password():
-    password = str(input("What is your password: "))
-    return password
-
-
-main()
+x = str(int('1.0'))
+x[-1] = '2'
---
+++ prac_4/warmup.py
@@ -0,0 +1,16 @@
+"""
+Numbers = [3, 1, 4, 1, 5, 9, 2,]
+Numbers[0] = 3
+Numbers[-1] = 2
+Numbers[3] = 1
+Numbers[:-1] = 3, 1, 4, 1, 5, 9
+Numbers[3:4] = 1,
+5 in numbers = true
+7 in numbers = false
+"3" in numbers = false
+Numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+Numbers[0] = "Ten"
+numbers[-1] = 1
+numbers[1:6]
+9 in numbers
+"""
